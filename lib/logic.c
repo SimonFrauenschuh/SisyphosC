8f6dbc051f67a446bb4426831e8ffd1ed4b32d84
SimonFrauenschuh <simon.frauenschuh@icloud.com>
2023-02-02 10:03:48 +0100

    changed PD-Values

--- lib/logic.c
+++ lib/logic.c
@@ -6,46 +6,112 @@
  * cannot be prosecuted for it
  */
 #include <sys/time.h>
+#include <math.h>
+#include <pthread.h>
 #include "touchpanel.h"
+#include "servo.h"
+#include "database.h"
 
 #pragma once
 
-// Logic for mode single-point (define a point, where the ball should move to)
-void moveToPoint(int xEst, int yEst) {
-    // Touchpanel-specific values for the center
-	int xMid = 7800, yMid = 7400;
+
 	int touchpanelPositionX, touchpanelPositionY;
 	int touchpanelPositionXOld, touchpanelPositionYOld;
+
+// Logic for mode single-point (define a point, where the ball should move to)
+void moveToPoint(int xEst, int yEst) {
+	int xDiff, yDiff;
 	// Values for the PD-Regulator
-	double dX, dY, pX, pY;
+	int dX = 0, dY = 0, pX = 0, pY = 0;
 
 	// Used for the "D" Part of the PD-Controller
 	struct timeval begin, end;
-	long microseconds;
+	long milliseconds;
 
 	// Start measuring time
     gettimeofday(&begin, 0);
 
-	while (1) {	
-		touchpanelPositionXOld = touchpanelPositionX;
-		touchpanelPositionYOld = touchpanelPositionY;
+	touchpanelPositionXOld = touchpanelPositionX;
+	touchpanelPositionYOld = touchpanelPositionY;
 
-		getTouchpanelPosition(&touchpanelPositionX, &touchpanelPositionY);
-		printf("xPosition: %d    |    yPosition: %d\n", touchpanelPositionX, touchpanelPositionY);
+	getTouchpanelPositionADC(&touchpanelPositionX, &touchpanelPositionY);
+	//getTouchpanelPositionUSB(&touchpanelPositionX, &touchpanelPositionY);
+	//writeDatabaseXY(touchpanelPositionX, touchpanelPositionY);
 
-		// Stop measuring time and calculate the elapsed time
-    	gettimeofday(&end, 0);
-    	microseconds = end.tv_usec - begin.tv_usec;
-		// Calculate the "D" Part of the PD-Controller
-		dX = (touchpanelPositionXOld - touchpanelPositionX) / (microseconds / 730.0);
-		dY = - (touchpanelPositionYOld - touchpanelPositionY) / (microseconds / 730.0);
-		// Start measuring time
-    	gettimeofday(&begin, 0);
+	// Control, if there isn't a misread value from the touchpad
+	// Sometimes, the touchpanel has a state, where "0" is sent for the x-Coordinate, no matter, what the real value is
+	if (touchpanelPositionX != 0 && touchpanelPositionY != 0) {
+		// Single values, that don't match, get ignored
+		if (!((touchpanelPositionX < touchpanelPositionXOld * 0.9) || (touchpanelPositionX > touchpanelPositionXOld * 1.1) || (touchpanelPositionY < touchpanelPositionYOld * 0.9) || (touchpanelPositionY > touchpanelPositionYOld * 1.1))) {
+			printf("xPosition: %d	|	yPosition: %d\n", touchpanelPositionX, touchpanelPositionY);
+			
+			// Stop measuring time and calculate the elapsed time
+			gettimeofday(&end, 0);
+			milliseconds = (end.tv_usec - begin.tv_usec) / 1000;
+			// Start measuring time
+			gettimeofday(&begin, 0);
 
-		pX = 408 - (double)(touchpanelPositionX - xMid) / 490;
-		pY = 385 + (double)(touchpanelPositionY - yMid) / 900;
+			xDiff = touchpanelPositionX - xEst;
+			// Add a offset and calculate the p-Part
+			pX = (xDiff > 0) ? (4 + xDiff / 15) : (-4 + xDiff / 15);
 
-		pwmWrite(PIN_BASE + 1, pX + dX);
-		pwmWrite(PIN_BASE + 0, pY + dY);
+
+			yDiff = touchpanelPositionY - yEst;
+			pY = (yDiff > 0) ? (5 + yDiff / 10) : (-5 + yDiff / 10);
+			
+			// Calculate the "D" Part of the PD-Controller
+			/*if (xDiff > 50 && xDiff < 50) {
+				dX = -15 * (touchpanelPositionXOld - touchpanelPositionX) / milliseconds;
+			}*/
+			xDiff = touchpanelPositionXOld - touchpanelPositionX;
+			//dX = (xDiff < 0) ? (2 + 16 * xDiff / milliseconds) : (-2 - 16 * xDiff / milliseconds); 
+			dX = (xDiff < 0) ? (-2 -16 * xDiff / milliseconds) : (2 + 16 * xDiff / milliseconds); 
+
+			yDiff = touchpanelPositionYOld - touchpanelPositionY;
+			dY = (yDiff < 0) ? (2 - 15 * yDiff / milliseconds) : (-2 - 15 * yDiff / milliseconds);
+							
+			if (pX + dX > 25) {
+				pX = 15;
+				dX = 10;
+			} else if (pX + dX < -25) {
+				pX = -15;
+				dX = -10;
+			}
+			if (pY + dY > 20) {
+				pY = 15;
+				dY = 10;
+			} else if (pY + dY < -20) {
+				pY = -15;
+				dY = -10;
+			}
+
+			pwmWrite(PIN_BASE + 0, CHANNEL0_MID - (pY + dY));
+			pwmWrite(PIN_BASE + 1, CHANNEL1_MID + (pY + dY));
+			pwmWrite(PIN_BASE + 2, CHANNEL2_MID + (pX + dX));
+			pwmWrite(PIN_BASE + 3, CHANNEL3_MID - (pX + dX));
+
+			printf("X: %d, %d\n", pX, dX);
+			printf("Y: %d, %d\n\n", pY, dY);
 		}
+	}
+}
+
+// Synchronize with the angle, given by the database (the phones gyroscope)
+void moveToAngle(int xEst, int yEst) {
+	xEst /= 2;
+	yEst /= 2;
+	if (xEst > 20) {
+		xEst = 20;
+	} else if (xEst < -20) {
+		xEst = -20;
+	}
+	if (yEst > 20) {
+		yEst = 20;
+	} else if (yEst < -20) {
+		yEst = -20;
+	}
+	setServoDegree(0, xEst);
+	setServoDegree(1, xEst);
+	setServoDegree(2, -yEst);
+	setServoDegree(3, -yEst);
 }
